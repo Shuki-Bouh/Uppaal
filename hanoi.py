9d306ca65471849edae3e4d588db75c8036ca738
Cyril <cyrilmaisani@yahoo.com>
2024-12-18 10:24:29 +0100

    Add many things

--- hanoi.py
+++ hanoi.py
@@ -1,5 +1,20 @@
 from parcours_graphe import RootedGraph
 
+class HanoiNode:
+    def __init__(self, state_disks, state_piliers):
+        self.state_disks = state_disks
+        self.state_piliers = state_piliers
+        return
+
+    def __eq__(self, other):
+        return
+
+    def __hash__(self):
+        return 1
+
+
+
+
 class Hanoi(RootedGraph):
 
     def __init__(self, nb_disk):
@@ -21,6 +36,22 @@ class Hanoi(RootedGraph):
 
         return True
 
+    def move(self, node, pilier, disk):
+        state_disks = node[0].copy()
+        state_piliers = node[1].copy()
+
+        prev_pilier = state_disks[disk]
+
+        state_disks[disk] = pilier
+        state_piliers[pilier] = disk
+
+        for d in state_disks[disk:]:
+            if state_disks[d] == prev_pilier:
+                state_piliers[prev_pilier] = d
+                break
+
+        return HanoiNode(state_disks, state_piliers)
+
 
     def neighbours(self, node):
         """
@@ -30,8 +61,11 @@ class Hanoi(RootedGraph):
         Renvoie une liste de tous les états voisins de node
         """
         state_disque, state_pilier = node
+        possible_states = []
+
         for pilier in state_pilier:
             disque = state_pilier[pilier]
-            for j in range(self.nb_pilier):
-                if j !=pilier:
-                    self.check_move(node, j, disque)
+            for p in range(self.nb_pilier):
+                if p != pilier:
+                    if self.check_move(node, p, disque):
+                        possible_states.append(self.move(node, p, disque))
