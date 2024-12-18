582d766344880bc5bb4b4cd649131c3b273a2c44
Cyril <cyrilmaisani@yahoo.com>
2024-12-18 09:22:28 +0100

    Implement check_move

--- hanoi.py
+++ hanoi.py
@@ -8,18 +8,15 @@ class Hanoi:
         self.nb_pilier = 3
         return
 
-    def move(self, pilier_cible, disk):
+    def check_move(self, pilier_cible, disk):
+        """"""
 
         if disk >= self.nb_disk or pilier_cible >= self.nb_pilier:
-            return False
+            return False  # Cas qui ne doivent pas se produire
 
         for d in self.state_disks[:disk]:
-            if self.state_disks[d] == pilier_cible or self.state_disks[d] == self.state_disks[disk]:
+            if self.state_disks[d] == pilier_cible or self.state_disks[d] == self.state_disks[disk]:  # On regarde si un disque est au dessus ou si un disque plus petit est déjà là-bas
                 return False
 
-        try:
-            if self.state_piliers[pilier_cible] > disk:
-                self.state_piliers[pilier_cible] = disk
-        except KeyError:
-            self.state_piliers[pilier_cible] = disk
+        return True
 
