567b521163a859592348360b9eecd6ac184ee4d8
Cyril <cyrilmaisani@yahoo.com>
2024-12-18 09:28:29 +0100

    Fix check_move to add node

--- hanoi.py
+++ hanoi.py
@@ -2,20 +2,18 @@ class Hanoi:
 
     def __init__(self, nb_disk):
         self.nb_disk = nb_disk
-        self.state_disks = [0 for _ in range(nb_disk)]  # Position de chaque disque
-        self.state_piliers = {}  # On place pour chaque pilier la valeur du disque le plus haut (le plus petit sur ce pilier)
-        self.state_piliers[0] = 0 # Quand on commence la partie, le disque 0 est sur le pilier 0 et seul le pilier 0 est utilisé
         self.nb_pilier = 3
         return
 
-    def check_move(self, pilier_cible, disk):
-        """"""
+    def check_move(self, node, pilier_cible, disk):
+
+        state_disks, state_piliers = node  # node = ([list_state_disks], {dict_state_pilier})
 
         if disk >= self.nb_disk or pilier_cible >= self.nb_pilier:
             return False  # Cas qui ne doivent pas se produire
 
-        for d in self.state_disks[:disk]:
-            if self.state_disks[d] == pilier_cible or self.state_disks[d] == self.state_disks[disk]:  # On regarde si un disque est au dessus ou si un disque plus petit est déjà là-bas
+        for d in state_disks[:disk]:
+            if state_disks[d] == pilier_cible or state_disks[d] == state_disks[disk]:  # On regarde si un disque est au dessus ou si un disque plus petit est déjà là-bas
                 return False
 
         return True
