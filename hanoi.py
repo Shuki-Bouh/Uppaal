7606f1d23cfec0b9dcacf07dd3a641904e8e3779
Nova66677 <eliott.chopin@ensta-bretagne.org>
2024-12-18 09:35:48 +0100

    adding neighbours function

--- hanoi.py
+++ hanoi.py
@@ -1,4 +1,6 @@
-class Hanoi:
+from parcours_graphe import RootedGraph
+
+class Hanoi(RootedGraph):
 
     def __init__(self, nb_disk):
         self.nb_disk = nb_disk
@@ -9,7 +11,6 @@ class Hanoi:
         return
 
     def move(self, pilier_cible, disk):
-
         if disk >= self.nb_disk or pilier_cible >= self.nb_pilier:
             return False
 
@@ -23,3 +24,17 @@ class Hanoi:
         except KeyError:
             self.state_piliers[pilier_cible] = disk
 
+
+    def neighbours(self, node):
+        """
+        Cette fonction créé tous les mouvement possibles à partir d'un état
+        Elle utilise la fonction check_move pour verifier si un deplacement est possible 
+        ou non
+        Renvoie une liste de tous les états voisins de node
+        """
+        state_disque, state_pilier = node
+        for pilier in state_pilier:
+            disque = state_pilier[pilier]
+            for j in range(self.nb_pilier):
+                if j !=pilier:
+                    self.check_move(node, j, disque)
