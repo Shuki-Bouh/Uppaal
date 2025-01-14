db8bb0817efef36da17e7eba85262def81bb95c7
shuki <cyrilmaisani@yahoo.com>
2025-01-14 17:57:14 +0100

    Add alice et bob

--- hanoiRootedRelation.py
+++ hanoiRootedRelation.py
@@ -2,29 +2,21 @@ from rootedRelation import RootedRelation
 from hanoi import Hanoi, HanoiNode
 
 
-class HanoiRootedRelation(RootedRelation):
+class HanoiRootedRelation(RootedRelation, Hanoi):
 
     def __init__(self, nb_disk: int):
-        super().__init__()
-        self.nb_disk = nb_disk
-        self.nb_pilier = 3
-        state_disks = [0 for _ in range(nb_disk)]
-        state_piliers = {0:0}
-        self.__root = [HanoiNode(state_disks, state_piliers)]
-
+        RootedRelation().__init__()
+        Hanoi.__init__(self, nb_disk)
     
     def initial(self):
         return self.__root
 
     def actions(self, node: HanoiNode) -> list:
         liste_config = []
-        for i in range(self.nb_pilier):
-            for j in range(self.nb_pilier):
-                if j in node.state_piliers and i in node.state_piliers :
-                    if node.state_piliers[i] < node.state_piliers[j]: 
-                        liste_config.append((i, j))
-                elif j not in node.state_piliers and i in node.state_piliers:
-                    liste_config.append((i, j))
+        for disk in range(self.nb_disk):
+            for pilier in range(self.nb_pilier):
+                if self.check_move(node, disk, pilier):
+                        liste_config.append((disk, pilier))
 
         return liste_config
 
@@ -34,5 +26,4 @@ class HanoiRootedRelation(RootedRelation):
         for action in actions:
             disk, pilier = action
             new_node = hanoi.move(node, pilier, disk)
-
             return new_node
