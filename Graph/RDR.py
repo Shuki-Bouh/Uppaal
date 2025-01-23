9ab87a16e37e0e0582552eb398954bdfb2b757c1
shuki <cyrilmaisani@yahoo.com>
2025-01-23 16:15:02 +0100

    Correction des fonctions de hash et eq. Début de documentation pour s'y retrouver ...

--- Graph/RDR.py
+++ Graph/RDR.py
@@ -15,3 +15,20 @@ class RootedDependentRelation(ABC):
         target = c.copy()
         a.behavior(i, target)
         return [target]
+
+
+class SoupDependantSemantics(ABC):
+
+    @abstractmethod
+    def initial(self):
+        pass
+
+    @abstractmethod
+    def actions(self, i, c):
+        return list(filter(lambda p: p.guard(i, c), self.program.pieces))
+
+    @abstractmethod
+    def execute(self, a, i, c):
+        target = c.copy()
+        a.behavior(i, target)
+        return [target]
